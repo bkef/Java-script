d33893fdd216f7cd8fc35368bbe19a78835770bc
bahareh kefayati <56130118+bkef@users.noreply.github.com>
2019-11-12 12:43:15 -0800

    Add files via upload

---
+++ logic/JS/main.js
@@ -0,0 +1,83 @@
+	
+
+	
+	alert("hello");
+
+	// function ifRain()
+	// {
+	// 	 let inputRain = prompt("Is It Rainning? 1-yes / 2-no");
+
+	// 	if (inputRain == 1){
+	// 		alert("Take an umnrella");
+	// 	} else { 
+	// 		alert ("do not take an umbrella");
+	// 	}
+	// }
+
+	// function ifRain()
+	// {
+	// 	let inputRain = prompt("is it rainning? 1-yes / 2-no / 3- live in vancouver");
+
+	// 	if (inputRain ==1 || inputRain ==3){
+	// 		alert("take an umbrella");
+	// 	} else {
+	// 		alert("dont take an umbrella");
+
+	// 	}
+
+
+	// }
+
+function Rain(){
+ 	let Rain = parseInt(prompt("is it rainnig? 1-yes / 2-no / 3-we live in vancouver / 4-it is snowing / 5- it is sunny"),10);
+
+ 	switch(Rain)
+ 	{
+ 		case 1: alert("take an umbrella");
+ 		break;
+ 		case 2: alert("dont take an umbrella");
+ 		break;
+ 		case 3: alert("always take an umbrella");
+ 		break;
+ 		case 4: alert(" make a snowman");
+ 		break;
+ 		case 5: alert("wear swimsuit");
+ 		break;
+ 		default: Rain();
+ 	}
+ }
+
+ // function fruitgame(){
+ // 	let fruit = "apple";
+ // 	let inputfruit = prompt("what is your favorite fruit?");
+ // 	if(inputfruit.toLowerCase() === fruit){
+ // 		console.log("you win!!");
+ // 	}else{
+ // 		console.log("you lost");
+ // 	}
+ // }
+
+ // function printnumber(){
+ // 	let i;
+ // 	for (let i = 0; i<=10 ; i+=1) {
+ // 		console.log(i);
+ // 	}
+
+ // }
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+i	
